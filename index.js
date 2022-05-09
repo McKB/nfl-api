23d30262cf9eefdd5fd41f596ec4a965dafe68d8
McKB <mckennakb@comcast.net>
2022-05-09 19:36:16 -0400

    sets up initial test routes

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 
+app.get('/team', (req, res) => {
+  return res.status(200).send('TO DO: /team controller')
+})
+
+app.get('/team/:id', (req, res) => {
+  return res.status(200).send('TO DO: /team/ID controller')
+})
+
 app.listen(1337, () => {
   console.log('listening on http://localhost:1337...') // eslint-disable-line no-console
 })
