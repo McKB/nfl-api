96da774dd322faf66e0c10eea05f3aa87c148578
McKB <mckennakb@comcast.net>
2022-05-09 19:47:41 -0400

    moved controllers to their own folder

---
+++ controllers/team.js
@@ -0,0 +1,9 @@
+const getAllTeams = (req, res) => {
+  return res.status(200).send('TO DO: /team controller')
+}
+
+const getTeamByID = (req, res) => {
+  return res.status(200).send('TO DO: /team/IDcontroller')
+}
+
+module.exports = { getAllTeams, getTeamByID }
--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express')
+const { getAllTeams, getTeamByID } = require('./controllers/team')
 const app = express()
 
-app.get('/team', (req, res) => {
-  return res.status(200).send('TO DO: /team controller')
-})
+app.get('/team', getAllTeams)
 
-app.get('/team/:id', (req, res) => {
-  return res.status(200).send('TO DO: /team/ID controller')
-})
+app.get('/team/:id', getTeamByID)
 
 app.listen(1337, () => {
   console.log('listening on http://localhost:1337...') // eslint-disable-line no-console
