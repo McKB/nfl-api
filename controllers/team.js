d443d659909cc5c1dc0d1b36f8962883def58c90
McKB <mckennakb@comcast.net>
2022-05-09 20:45:18 -0400

    getTeamByID is now functioning

--- controllers/team.js
+++ controllers/team.js
@@ -5,7 +5,14 @@ const getAllTeams = (req, res) => {
 }
 
 const getTeamByID = (req, res) => {
-  return res.status(200).send('TO DO: /team/IDcontroller')
+  const id = parseInt(req.params.id)
+  const team = teams.find((team) => team.id === id)
+
+  if (team) {
+    return res.status(200).send(team)
+  } else {
+    return res.status(404).send('Team not found :/')
+  }
 }
 
 module.exports = { getAllTeams, getTeamByID }
